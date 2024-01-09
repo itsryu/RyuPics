b4c0fbe4fbbcb020e99cd8d2f95e3d466f64ecc9
Ryuzaki <joaovictor9162@outlook.com>
2024-01-09 19:33:45 -0300

    updating meta

--- src/routes/ImageController.ts
+++ src/routes/ImageController.ts
@@ -16,21 +16,21 @@ class ImageController {
             const imageId = req.params.id;
             const image = await collection.findOne({ _id: new ObjectId(imageId) });
 
-            if (!image) {
+            if (image) {
+                const htmlTemplate = readFileSync(join(__dirname, '../../../', 'public', 'index.html'), 'utf-8');
+
+                const html = htmlTemplate
+                    .replace('{{imageId}}', imageId)
+                    .replace('{{imageTitle}}', 'Título da Imagem')
+                    .replace('{{imageDescription}}', 'Descrição da Imagem')
+                    .replace('{{imageUrl}}', `${process.env.STATE == 'development' ? `${process.env.LOCAL_URL}:${process.env.PORT}/image/${imageId}` : `${process.env.DOMAIN_URL}/image/${imageId}`}`)
+                    .replace('{{imageContentType}}', image.contentType)
+                    .replace('{{imageData}}', image.data.toString('base64'));
+
+                res.send(html);
+            } else {
                 return res.status(404).json({ code: '404', message: 'Image not found' });
             }
-
-            const htmlTemplate = readFileSync(join(__dirname, '../../../', 'public', 'index.html'), 'utf-8');
-
-            const html = htmlTemplate
-                .replace('{{imageId}}', imageId)
-                .replace('{{imageTitle}}', 'Título da Imagem')
-                .replace('{{imageDescription}}', 'Descrição da Imagem')
-                .replace('{{imageUrl}}', `${process.env.STATE == 'development' ? `${process.env.LOCAL_URL}:${process.env.PORT}/image/${imageId}` : `${process.env.DOMAIN_URL}/image/${imageId}`}`)
-                .replace('{{imageContentType}}', image.contentType)
-                .replace('{{imageData}}', image.data.toString('base64'));
-
-            res.send(html);
         } catch (err) {
             res.status(500).json({ code: '500', message: 'Incorrect imageId format' });
         }
